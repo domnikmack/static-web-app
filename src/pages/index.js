3f1cee1b00c314c25ed1180cc1d528a6ae0d95a5
Domnik Mack <domnikmack@gmail.com>
2018-06-21 23:33:11 -0400

    CSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,18 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () =>
+<div style={{margin: '3rem auto', padding: '2rem', maxWidth: 800 }}>
+  <h1>Hello world!</h1>
+  <h2>Eiusmod ex amet</h2>
+  <p>
+    Esse nulla esse ipsum consequat in ex commodo. Incididunt sunt reprehenderit voluptate voluptate consectetur. Qui dolor ea deserunt dolore labore sint ullamco duis velit commodo. Deserunt minim do sit eiusmod ullamco officia veniam quis. Ea nulla duis sunt <a href="#" >eiusmod</a>.
+  </p>
+  <blockquote>
+    Excepteur tempor non ea ullamco ex ea labore elit duis mollit sint.
+    </blockquote>
+  <p>
+    Id fugiat cupidatat adipisicing consequat pariatur sit cillum. Dolor reprehenderit ex fugiat nostrud eu. Anim sunt ut nulla voluptate. Non do laboris laboris commodo ad tempor fugiat labore ut laboris non. Consectetur nostrud ea cupidatat est cillum eiusmod velit. Aliquip sint velit consectetur nostrud ullamco labore laboris sunt deserunt nisi mollit sit ut cupidatat.
+  </p>
+
+
+  </div>
